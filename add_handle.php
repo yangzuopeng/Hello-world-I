f8e704c74972414d709e6ed916a03b9bd24f38f7
yangzuopeng <775405239@qq.com>
2017-03-19 00:53:00 +0800

    Update add_handle.php

--- add_handle.php
+++ add_handle.php
@@ -1,8 +1,8 @@
 <?php
 require_once '/connect.php';
-$name=$_POST["name"];
-$content=$_POST["content"];
-$price=$_POST["price"];
+$name=$_POST['name'];
+$content=$_POST['content'];
+$price=$_POST['price'];
 echo $name;
 $sql="INSERT INTO library(name,content,price) VALUES('$name','$content',$price)";
 if(mysqli_query($con,$sql))
